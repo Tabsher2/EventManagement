eadaf32dc7ecd2553120e9ebd81e3b789bb86472
Tabsher2 <absher.trevor@gmail.com>
2017-03-27 22:48:43 -0400

    Started Create University
    Got most of the Create University Code working. It's currently hardcoded
    to user 3 since login isn't working yet and there are also currently no
    images.

---
+++ EventJS/university.js
@@ -0,0 +1,60 @@
+var app = angular.module('universityApp', []).controller('universityViewModel', function($scope, $http) {
+    
+	/***************************
+	*** Variable Declaration ***
+	***************************/
+	
+	$scope.users = [];
+	$scope.taken = false;
+	$scope.invalid = false;
+	$scope.userID = 3;
+	
+	/****************************
+	**** Function Prototypes ****
+	*****************************/
+	
+	$scope.createUniversity = createUniversity;
+	
+	function createUniversity() {
+		var flag = false;
+		var name = document.getElementById('name').value;
+		var address = document.getElementById('location').value;
+		var description = document.getElementById('description').value;
+		var numStudents = document.getElementById('numStudents').value;
+		var universityAdmin = $scope.userID;
+		//check valid
+		
+		var url = '/EventJSON/AddUniversity.aspx';
+		
+//		alert( "" + email + " -- " + username );
+
+		var dt = '{\"name\":\"' + name + '\",\"location\": \"' + address + '\",\"description\": \"' + 
+					description + '\",\"numStudents\": \"' + numStudents + '\",\"admin\": \"' + universityAdmin + '\"}';
+		
+		
+		$.ajax({
+			url: url,
+			type: 'POST',
+			dataType: 'json',
+			data: dt,
+			success: function(response) 
+			{
+				if(response.error === "1")
+				{
+					document.getElementById('feedback').innerHTML = "The name is already in use.";
+					document.getElementById('feedback').style.display = "block";
+				}
+				else
+				{
+					document.getElementById('send').style.display = "none";
+					document.getElementById('feedback').innerHTML = "University successfully made!";
+					document.getElementById('feedback').style.display = "block";
+				}
+			},
+			error: function(arg1, arg2, arg3) 
+			{
+				alert("Uh-Oh. Something went wrong.")
+			}
+		});
+	}
+});
---
+++ EventJSON/AddUniversity.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.IO;
+using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+public partial class JSONServices_AddUniversity : System.Web.UI.Page
+{
+
+	public struct UniversityRequest
+	{
+		public string name;
+		public string location;
+		public string description;
+		public int numStudents;
+		public int admin;
+	}
+	
+	public struct UniversityResponse
+	{
+		public string message;
+		public string error;
+	}
+
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		UniversityRequest request;
+		UniversityResponse response = new UniversityResponse();
+		response.error = String.Empty;
+
+		// Need passed in store id and number of requested results.
+		// 1. Deserialize the incoming Json.
+		try
+		{
+			request = GetRequestInfo();
+			if( request.name == null || request.location == null || request.description == null || request.numStudents == null ){
+				response.error = "University not created";
+				SendInfoAsJson(response);
+				
+				return;
+			}
+		}
+		catch (Exception ex)
+		{
+			response.error = ex.Message.ToString();
+
+			// Return the results as Json.
+			SendInfoAsJson(response);
+
+			return;
+		}
+
+		// Do stuff here.
+		SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+		try
+		{
+			connection.Open();
+			
+			string sql = String.Format("SELECT * FROM University WHERE name=@un");
+			SqlCommand command = new SqlCommand( sql, connection );
+			command.Parameters.Add(new SqlParameter("@un", request.name));
+			SqlDataReader reader = command.ExecuteReader();
+			if( reader.Read() )
+			{
+				response.error = "1";
+			}
+			
+			if( response.error != "")
+			{
+				SendInfoAsJson(response);
+				return;
+			}
+
+			
+			/*
+#if CRAP	
+			response.error = sql;
+			SendInfoAsJson(response);
+			return;
+#endif			
+			*/
+		}
+		catch (Exception ex)
+		{
+			response.error = ex.Message.ToString();
+		}
+		finally
+		{
+			if (connection.State == ConnectionState.Open)
+			{
+				connection.Close();
+			}
+		}
+		
+		try
+		{
+			connection.Open();
+			string sql = String.Format("INSERT into University (name,location,description,numStudents, superAdmin) VALUES ('{0}','{1}','{2}', '{3}', '{4}')", request.name, request.location, request.description, request.numStudents, request.admin);
+			SqlCommand command2 = new SqlCommand( sql, connection );
+			command2.ExecuteNonQuery();
+			response.message = "Successfully added university";
+
+		}
+		catch (Exception ex)
+		{
+			response.error = ex.Message.ToString();
+		}
+		finally
+		{
+			if (connection.State == ConnectionState.Open)
+			{
+				connection.Close();
+			}
+		}
+		
+		SendInfoAsJson(response);
+	}
+
+	UniversityRequest GetRequestInfo()
+	{
+		// Get the Json from the POST.
+		string strJson = String.Empty;
+		HttpContext context = HttpContext.Current;
+		context.Request.InputStream.Position = 0;
+		using (StreamReader inputStream = new StreamReader(context.Request.InputStream))
+		{
+			strJson = inputStream.ReadToEnd();
+		}
+
+		// Deserialize the Json.
+		UniversityRequest request = JsonConvert.DeserializeObject<UniversityRequest>(strJson);
+
+		return (request);
+	}
+
+	void SendInfoAsJson(UniversityResponse response)
+	{
+		string strJson = JsonConvert.SerializeObject(response);
+		Response.ContentType = "application/json; charset=utf-8";
+		Response.Write(strJson);
+		Response.End();
+	}
+}
